907798ab27165606e0e4a15dc12c10b526400f2c
Eric Yang <eric@EricsMac.local>
2017-08-13 22:10:39 +1200

    x

--- tabNumber.js
+++ tabNumber.js
@@ -18,8 +18,22 @@ function updateTitleNumber() {
 	 chrome.tabs.getAllInWindow(null, function(tabs){
 	    for (var i = 0; i < tabs.length; i++) {
 	    	var tab = tabs[i]
+	    	// console.log("tab.url.matchw")
+	    	// console.log("tab.url.match" + (tab.url.match("") == null))
 
-	    	chrome.tabs.executeScript(tab.id,{code:"document.title = 'My lame title!'"});
+
+	    	if (tab.url.match("^chrome") == null) {
+	    		console.log("title: " + tab.title + " url:" + tab.url)
+	    		var newTitle = "document.title = '1'" + tab.title
+	    		chrome.tabs.executeScript(tab.id,{
+	    			code: newTitle
+	    		});
+	    	}
+	    	
 	    }
 	});
 }
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+   // console.log(changeInfo);
+});
