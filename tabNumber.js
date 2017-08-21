a2c26b82692114b1488dbfe42311c557763c57c6
Eric Yang <eric@EricsMac.local>
2017-08-21 22:49:02 +1200

    Can show tab number well

--- tabNumber.js
+++ tabNumber.js
@@ -12,22 +12,46 @@ chrome.tabs.onRemoved.addListener(function(){
 	updateTitleNumber()
 });
 
-
-
 function updateTitleNumber() {
 	 chrome.tabs.getAllInWindow(null, function(tabs){
 	    for (var i = 0; i < tabs.length; i++) {
 	    	var tab = tabs[i]
 	    	// console.log("tab.url.matchw")
 	    	// console.log("tab.url.match" + (tab.url.match("") == null))
-
-
+	    	
 	    	if (tab.url.match("^chrome") == null) {
+	    		var title_spliter = "_"
+
 	    		console.log("title: " + tab.title + " url:" + tab.url)
-	    		var newTitle = "document.title = '1'" + tab.title
+	    		
+	    		var originalTitle = tab.title
+	    		var oldTitle = tab.title
+	    		var tabNumber = i + 1
+
+				var matchedNumbers = tab.title.match("^[0-9]+(?!-_)")
+				if (matchedNumbers != null && matchedNumbers.length > 0) {
+		    		var matchedNumber = matchedNumbers[0]
+		    		if (matchedNumber != null && !isNaN(matchedNumber)) {
+		    			console.log("Matched number: " + matchedNumber)
+		    			let oldTabNumber = parseInt(matchedNumber)
+						if (oldTabNumber == null || oldTabNumber<0) {
+							console.log("Invalidated tab number")
+						} else {
+							var titlePrefix = matchedNumber + "_"
+
+							originalTitle = tab.title.substring(titlePrefix.length)
+							console.log("originalTitle: " + originalTitle)
+						}
+		    		}
+				}
+
+				var newTitle = "document.title = '" + tabNumber + title_spliter + originalTitle + "'"
+	    		console.log(newTitle)
+
 	    		chrome.tabs.executeScript(tab.id,{
 	    			code: newTitle
 	    		});
+
 	    	}
 	    	
 	    }
@@ -35,5 +59,6 @@ function updateTitleNumber() {
 }
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-   // console.log(changeInfo);
+   console.log("chrome.tabs.onUpdated.addListener title: " + changeInfo.title);
+   updateTitleNumber()
 });
